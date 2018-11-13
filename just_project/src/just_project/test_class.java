195da303ecca5dcfd36ca828d763674ea58fa208
0130103 <tna013@daum.net>
2018-11-13 18:58:46 +0900

    test code . . .

---
+++ just_project/src/just_project/test_class.java
@@ -0,0 +1,12 @@
+package just_project;
+
+public class test_class {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("test code");
+
+	}
+
+}
